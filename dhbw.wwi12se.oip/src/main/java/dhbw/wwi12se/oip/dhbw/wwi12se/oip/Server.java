77af6422d4e5acf871fdcdbe7aed3b8889c9d889
npartsch <nicolas.partsch@gmx.de>
2015-03-10 12:27:21 +0100

    Generisches erzeugen von MenitoredDataItems

--- dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
@@ -2,6 +2,7 @@ package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -54,32 +55,23 @@ public class Server {
 		target = client.getAddressSpace().getNamespaceTable()
 				.toNodeId(r.getNodeId());
 
-		NodeId target1 = new NodeId(5, "Random1");
-		NodeId target2 = new NodeId(5, "Counter1");
-		NodeId target3 = new NodeId(5, "Expression1");
-		NodeId target4 = new NodeId(5, "Square1");
+		ArrayList<NodeId> targets = new ArrayList<NodeId>();
+		targets.add(new NodeId(5, "Random1"));
+		targets.add(new NodeId(5, "Counter1"));
+		targets.add(new NodeId(5, "Expression1"));
+		targets.add(new NodeId(5, "Square1"));
 
 		Subscription subscription = new Subscription();
-
-		MonitoredDataItem item1 = new MonitoredDataItem(target1,
-				Attributes.Value, MonitoringMode.Reporting);
-
-		MonitoredDataItem item2 = new MonitoredDataItem(target2,
-				Attributes.Value, MonitoringMode.Reporting);
-
-		MonitoredDataItem item3 = new MonitoredDataItem(target3,
-				Attributes.Value, MonitoringMode.Reporting);
-
-		MonitoredDataItem item4 = new MonitoredDataItem(target4,
-				Attributes.Value, MonitoringMode.Reporting);
-
-		subscription.addItem(item1);
-		subscription.addItem(item2);
-		subscription.addItem(item3);
-		subscription.addItem(item4);
+		ArrayList<MonitoredDataItem> items = new ArrayList<MonitoredDataItem>();
+		for (NodeId nodeId : targets) {
+			MonitoredDataItem dataItem = new MonitoredDataItem(nodeId,
+					Attributes.Value, MonitoringMode.Reporting);
+			subscription.addItem(dataItem);
+			items.add(dataItem);
+		}
 		client.addSubscription(subscription);
 
-		item1.setDataChangeListener(new MonitoredDataItemListener() {
+		items.get(0).setDataChangeListener(new MonitoredDataItemListener() {
 
 			public void onDataChange(MonitoredDataItem arg0, DataValue arg1,
 					DataValue arg2) {
