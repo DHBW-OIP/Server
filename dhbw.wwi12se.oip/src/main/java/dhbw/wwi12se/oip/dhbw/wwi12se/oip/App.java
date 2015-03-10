e3a8a66f507c51f06aee0e686de21d1082eb0959
Daniel.Hartmann <daniel.hartmann@unitcon.com>
2015-03-10 10:05:19 +0100

    initial

---
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/App.java
@@ -0,0 +1,13 @@
+package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
