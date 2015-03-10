680bffb96c6a23d57782d5a624501dd1e8ae34e9
Daniel.Hartmann <daniel.hartmann@unitcon.com>
2015-03-10 10:35:32 +0100

    server

--- dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
@@ -1,5 +1,56 @@
 package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Locale;
+
+import org.opcfoundation.ua.builtintypes.LocalizedText;
+import org.opcfoundation.ua.core.ApplicationDescription;
+import org.opcfoundation.ua.core.ApplicationType;
+import org.opcfoundation.ua.transport.security.SecurityMode;
+
+import com.prosysopc.ua.ApplicationIdentity;
+import com.prosysopc.ua.SecureIdentityException;
+import com.prosysopc.ua.client.UaClient;
+
 public class Server {
+
+	public static void main() throws Exception {
+
+		// Create client object
+		UaClient client = new UaClient(
+				"opc.tcp://localhost:53530/OPCUA/SimulationServer");
+		client.setSecurityMode(SecurityMode.NONE);
+
+		initialize(client);
+		client.connect();
+
+	}
+
+	/**
+	 * Initialize the client
+	 * 
+	 * @param client
+	 * @throws SecureIdentityException
+	 * @throws IOException
+	 * @throws UnknownHostException
+	 */
+	protected static void initialize(UaClient client)
+			throws SecureIdentityException, IOException, UnknownHostException {
+		// *** Application Description is sent to the server
+		ApplicationDescription appDescription = new ApplicationDescription();
+		appDescription.setApplicationName(new LocalizedText("DHBW Client",
+				Locale.GERMAN));
+
+		// 'localhost' (all lower case) in the URI is converted to the actual
+		// host name of the computer in which the application is run
+		appDescription.setApplicationUri("urn:localhost:UA:DHBWClient");
+		appDescription.setProductUri("urn:prosysopc.com:UA:DHBWClient");
+		appDescription.setApplicationType(ApplicationType.Client);
+
+		final ApplicationIdentity identity = new ApplicationIdentity();
+		identity.setApplicationDescription(appDescription);
+		client.setApplicationIdentity(identity);
+	}
 
 }
