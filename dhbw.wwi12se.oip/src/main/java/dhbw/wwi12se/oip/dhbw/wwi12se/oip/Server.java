67cf8c12a7ac263bb32d14aaffb3a74cd39f6297
Daniel.Hartmann <daniel.hartmann@unitcon.com>
2015-03-10 11:30:49 +0100

    sirukjnm

--- dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/Server.java
@@ -2,20 +2,30 @@ package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Locale;
 
+import org.opcfoundation.ua.builtintypes.DataValue;
 import org.opcfoundation.ua.builtintypes.LocalizedText;
+import org.opcfoundation.ua.builtintypes.NodeId;
 import org.opcfoundation.ua.core.ApplicationDescription;
 import org.opcfoundation.ua.core.ApplicationType;
+import org.opcfoundation.ua.core.Attributes;
+import org.opcfoundation.ua.core.Identifiers;
+import org.opcfoundation.ua.core.MonitoringMode;
+import org.opcfoundation.ua.core.ReferenceDescription;
 import org.opcfoundation.ua.transport.security.SecurityMode;
 
 import com.prosysopc.ua.ApplicationIdentity;
 import com.prosysopc.ua.SecureIdentityException;
+import com.prosysopc.ua.client.MonitoredDataItem;
+import com.prosysopc.ua.client.MonitoredDataItemListener;
+import com.prosysopc.ua.client.Subscription;
 import com.prosysopc.ua.client.UaClient;
 
 public class Server {
 
-	public static void main() throws Exception {
+	public static void main(String[] args) throws Exception {
 
 		// Create client object
 		UaClient client = new UaClient(
@@ -24,6 +34,43 @@ public class Server {
 
 		initialize(client);
 		client.connect();
+		DataValue value = client
+				.readValue(Identifiers.Server_ServerStatus_State);
+
+		client.getAddressSpace().setMaxReferencesPerNode(1000);
+		NodeId nid = Identifiers.RootFolder;
+
+		List<ReferenceDescription> references = client.getAddressSpace()
+				.browse(nid);
+
+		// Example of Namespace Browsing
+		NodeId target;
+		ReferenceDescription r = references.get(0);
+
+		target = client.getAddressSpace().getNamespaceTable()
+				.toNodeId(r.getNodeId());
+		references = client.getAddressSpace().browse(target);
+		r = references.get(4);
+		target = client.getAddressSpace().getNamespaceTable()
+				.toNodeId(r.getNodeId());
+
+		NodeId target2 = new NodeId(5, "Counter1");
+
+		Subscription subscription = new Subscription();
+		MonitoredDataItem item = new MonitoredDataItem(target2,
+				Attributes.Value, MonitoringMode.Reporting);
+
+		subscription.addItem(item);
+		client.addSubscription(subscription);
+
+		item.setDataChangeListener(new MonitoredDataItemListener() {
+
+			public void onDataChange(MonitoredDataItem arg0, DataValue arg1,
+					DataValue arg2) {
+				System.out.println(arg1);
+
+			}
+		});
 
 	}
 
