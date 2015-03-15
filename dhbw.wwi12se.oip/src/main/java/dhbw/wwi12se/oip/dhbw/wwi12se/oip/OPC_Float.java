f0e1db875ab4be44c6bf9a60374a0a7f6f9c85d7
Daniel.Hartmann <daniel.hartmann@unitcon.com>
2015-03-15 20:29:08 +0100

    JAXB

--- dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/OPC_Float.java
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/OPC_Float.java
@@ -1,10 +1,19 @@
 package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.opcfoundation.ua.builtintypes.DataValue;
 
+@XmlRootElement
 public class OPC_Float extends OPC {
 
+	@XmlElement
 	private float _value;
+
+	public OPC_Float() {
+		super(null);
+	}
 
 	public OPC_Float(DataValue arg) {
 		super(arg);
--- dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/OPC_Integer.java
+++ dhbw.wwi12se.oip/src/main/java/dhbw/wwi12se/oip/dhbw/wwi12se/oip/OPC_Integer.java
@@ -1,10 +1,19 @@
 package dhbw.wwi12se.oip.dhbw.wwi12se.oip;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.opcfoundation.ua.builtintypes.DataValue;
 
+@XmlRootElement
 public class OPC_Integer extends OPC {
 
+	@XmlElement
 	private int _value;
+
+	public OPC_Integer() {
+		super(null);
+	}
 
 	public OPC_Integer(DataValue arg) {
 		super(arg);
