491d4320a5991208fc1df4fe67f16a63b0e2e332
agp <teddfan@gmail.com>
2015-04-08 14:12:55 +0200

    Diesmal echt die AppConfig :D

---
+++ dhbw.wwi12se.oip/src/main/java/Stuff/AppSetting.java
@@ -0,0 +1,8 @@
+package Stuff;
+
+public class AppSetting
+{
+	public final static String[] sourceSystem = { "opc.tcp://localhost:53530/OPCUA/SimulationServer" };
+	public final static String queueName = "QUEUE2";
+	public final static String host = "localhost";
+}
